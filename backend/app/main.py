aa8a2962dedc554c121ed37ab008804ade599e52
Shadab1802 <121809519+Shadab1802@users.noreply.github.com>
2025-08-29 18:17:10 +0530

    adding model fetching and running

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-from api.v1 import home
+from backend.api.v1 import home,predictor
 
 app = FastAPI()
 
 #Includes Routers
 app.include_router(home.router,prefix="/api",tags=["info"])
-
+app.include_router(predictor.router,prefix="/api",tags=["predicting from model"])
 @app.get("/")
 def root():
     return {"message": "Welcome to API"}
