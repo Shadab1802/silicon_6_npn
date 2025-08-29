8575adb850ae87795797ce3560847033ace3004b
Shadab1802 <121809519+Shadab1802@users.noreply.github.com>
2025-08-29 22:45:52 +0530

    testing front and back mergingjjj

--- frontend/batch.py
+++ frontend/batch.py
@@ -2,6 +2,9 @@ import streamlit as st
 import requests
 import pandas as pd
 from io import BytesIO
+import os
+
+BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")  # default local
 
 st.header("Batch Prediction via CSV")
 uploaded_file = st.file_uploader("Upload CSV for batch prediction", type=["csv"], key="batch")
@@ -14,7 +17,7 @@ if uploaded_file is not None:
             try:
                 # Prepare file for request
                 files = {"file": (uploaded_file.name, uploaded_file, "text/csv")}
-                response = requests.post("http://127.0.0.1:8000/api/airlines", files=files)
+                response = requests.post(f"{BACKEND_URL}/api/airlines", files=files)
                 
                 if response.status_code == 200:
                     result = response.json()
