ae7992428ceb1967b5985faec655d4302b0b8130
Shadab1802 <121809519+Shadab1802@users.noreply.github.com>
2025-09-02 21:15:49 +0530

    test2 model_integration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,11 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 from typing import Optional
 
 class Settings(BaseSettings):
-    aws_access_key: Optional[str] = None
-    aws_secret_access_key: Optional[str] = None
-    aws_region: Optional[str] = None
+    aws_access_key: Optional[str] = Field(None, env="AWS_ACCESS_KEY")
+    aws_secret_access_key: Optional[str] = Field(None, env="AWS_SECRET_ACCESS_KEY")
+    aws_region: Optional[str] = Field(None, env="AWS_REGION")
+    bucket_name: Optional[str] = Field(None, env="BUCKET_NAME")
 
     class Config:
         env_file = ".env"
--- backend/app/services/fetcher.py
+++ backend/app/services/fetcher.py
@@ -1,5 +1,6 @@
 from app.core.s3 import get_s3_client
 import os
+from app.core.config import settings
 
 FILES = {
     "model": ("flight_model.pkl", "app/models/model.pkl"),
@@ -7,14 +8,14 @@ FILES = {
 }
 
 def download_models():
-    """Download model &encoder froom s3 if not alrady present"""
-    s3=get_s3_client()
+    """Download model & encoder from S3 if not already present"""
+    s3 = get_s3_client()
     local_paths = {}
 
     for key, (s3_path, local_path) in FILES.items():
         if not os.path.exists(local_path):
-            os.makedirs(os.path.dirname(local_path),exist_ok=True)
-            s3.download_file(os.getenv("BUCKET_NAME","None"),s3_path,local_path)
+            os.makedirs(os.path.dirname(local_path), exist_ok=True)
+            s3.download_file(settings.bucket_name, s3_path, local_path)
             print(f"✅ {key} downloaded from S3")
         else:
             print(f"✅ {key} already exists locally")
